e0cdb59dfc8e532d308f1fdd8f264be53bffc580
lxc-xx <lixuanchong@foxmail.com>
2013-09-26 17:46:26 -0400

    HW1 makefile

---
+++ HW1/CMakeFiles/HW1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HW1.dir/main.o"
+  "CMakeFiles/HW1.dir/reference_calc.o"
+  "CMakeFiles/HW1.dir/compare.o"
+  "HW1.pdb"
+  "HW1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
